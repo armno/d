ee2c8f1a0455993cd9783b2cba6ce01c65458301
Armno <monkeyarmno@gmail.com>
2018-07-22 20:01:40 +0700

    move logic to a function

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,37 @@ import fetch from 'isomorphic-unfetch';
 import config from '../config';
 import Link from 'next/link';
 
+async function getBikeActivities() {
+	const bikeID = config.STRAVA_BIKE_ID;
+	const apiKey = config.STRAVA_API_KEY;
+	const after = config.BIKE_SINCE;
+	const perPage = config.PER_PAGE;
+	const baseURL = `https://www.strava.com/api/v3/athlete/activities?after=${after}&per_page=${perPage}`;
+	const res = await fetch(baseURL, {
+		headers: {
+			Authorization: `Bearer ${apiKey}`
+		}
+	});
+	const activities = await res.json();
+	const activitiesByBike = activities.filter(activity => {
+		return activity.gear_id === bikeID && activity.type === 'Ride';
+	});
+	const summary = activitiesByBike.map(activity => {
+		return {
+			id: activity.id,
+			name: activity.name,
+			elevation: activity.total_elevation_gain,
+			distance: activity.distance,
+			time: activity.elapsed_time
+		};
+	});
+
+	return summary;
+}
+
 export default class extends React.Component {
 	static async getInitialProps({ req }) {
-		const profileID = config.STRAVA_PROFILE_ID;
-		const bikeID = config.STRAVA_BIKE_ID;
-		const API_KEY = config.STRAVA_API_KEY;
-		const after = config.BIKE_SINCE;
-
-		const baseURL = `https://www.strava.com/api/v3/athlete/activities?after=${after}&per_page=200`;
-		const res = await fetch(baseURL, {
-			headers: {
-				Authorization: `Bearer ${API_KEY}`
-			}
-		});
-		const activities = await res.json();
-		const activitiesByBike = activities.filter(activity => {
-			return activity.gear_id === bikeID && activity.type === 'Ride';
-		});
-		const summary = activitiesByBike.map(activity => {
-			return {
-				id: activity.id,
-				name: activity.name,
-				elevation: activity.total_elevation_gain,
-				distance: activity.distance,
-				time: activity.elapsed_time
-			};
-		});
+		const summary = await getBikeActivities();
 		console.table(summary);
 		return {
 			// elevation_gain: json.all_ride_totals.elevation_gain
